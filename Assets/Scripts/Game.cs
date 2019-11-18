460c229762cd9c359ecd65c2f5ab10478bd39afb
Chumplumps <47583539+Chumplumps@users.noreply.github.com>
2019-11-18 15:48:37 +1100

    Initial commit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public int score = 0;
+    public bool isGameOver = false;
+
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private GameObject startGameButton;
+    [SerializeField]
+    private Text gameOverText;
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private Text titleText;
+    [SerializeField]
+    private Spawner spawner;
+
+    private static Game instance;
+
+    private void Start()
+    {
+        instance = this;
+        titleText.enabled = true;
+        gameOverText.enabled = false;
+        scoreText.enabled = false;
+        startGameButton.SetActive(true);
+    }
+
+    public static void GameOver()
+    {
+        instance.titleText.enabled = true;
+        instance.startGameButton.SetActive(true);
+        instance.isGameOver = true;
+        instance.spawner.StopSpawning();
+        instance.player.GetComponent<Ship>().Explode();
+        instance.gameOverText.enabled = true;
+    }
+
+    public void NewGame()
+    {
+        isGameOver = false;
+        titleText.enabled = false;
+        startGameButton.SetActive(false);
+        player.transform.position = new Vector3(0, -3.22f, 0);
+        player.transform.eulerAngles = new Vector3(90, 180, 0);
+        score = 0;
+        scoreText.text = "SEARS: " + score;
+        scoreText.enabled = true;
+        spawner.BeginSpawning();
+        player.GetComponent<Ship>().RepairShip();
+        spawner.ClearAsteroids();
+        gameOverText.enabled = false;
+    }
+
+    public static void AsteroidDestroyed()
+    {
+        instance.score++;
+        instance.scoreText.text = "SEARS: " + instance.score;
+    }
+
+    public Ship GetShip()
+    {
+        return player.GetComponent<Ship>();
+    }
+
+    public Spawner GetSpawner()
+    {
+        return spawner.GetComponent<Spawner>();
+    }
+}
